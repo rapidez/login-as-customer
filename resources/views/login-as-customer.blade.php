0f664fe54cdef0c1e224babfa27109f827492640
indykoning <15870933+indykoning@users.noreply.github.com>
2024-12-13 11:08:44 +0100

    Rapidez v3 support (#13)

--- resources/views/login-as-customer.blade.php
+++ resources/views/login-as-customer.blade.php
@@ -8,11 +8,11 @@
     <div class="flex flex-col items-center">
         <h1 class="font-bold text-4xl mb-5">@lang('Login as customer')</h1>
 
-        <login-as-customer v-slot="{ inputChange, login }" v-cloak>
-            <form class="p-8 border rounded w-[400px]" v-on:submit.prevent="login">
-                <x-rapidez::input v-on:input="inputChange" name="username" label="Admin username" required/>
-                <x-rapidez::input v-on:input="inputChange" name="password" label="Admin password" type="password" required/>
-                <x-rapidez::input v-on:input="inputChange" name="customer" label="Customer email" type="email" required/>
+        <login-as-customer v-slot="login" v-cloak>
+            <form class="p-8 border rounded w-[400px]" v-on:submit.prevent="login.login">
+                <x-rapidez::input v-model="login.username" name="username" label="Admin username" required/>
+                <x-rapidez::input v-model="login.password" name="password" label="Admin password" type="password" required/>
+                <x-rapidez::input v-model="login.customer" name="customer" label="Customer email" type="email" required/>
                 <x-rapidez::button type="submit" class="mt-5">
                     @lang('Login as customer')
                 </x-rapidez::button>
