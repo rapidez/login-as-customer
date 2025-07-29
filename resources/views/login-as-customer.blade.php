046ec17bdfae1a4b728555aede0a5cfbb025f85d
Jade Geels <a@jgw.tf>
2025-07-29 09:37:35 +0200

    Form labels fix (#15)

--- resources/views/login-as-customer.blade.php
+++ resources/views/login-as-customer.blade.php
@@ -10,9 +10,18 @@
 
         <login-as-customer v-slot="login" v-cloak>
             <form class="p-8 border rounded w-[400px]" v-on:submit.prevent="login.login">
-                <x-rapidez::input v-model="login.username" name="username" label="Admin username" required/>
-                <x-rapidez::input v-model="login.password" name="password" label="Admin password" type="password" required/>
-                <x-rapidez::input v-model="login.customer" name="customer" label="Customer email" type="email" required/>
+                <label>
+                    <x-rapidez::label>@lang('Admin username')</x-rapidez::label>
+                    <x-rapidez::input v-model="login.username" name="username" required/>
+                </label>
+                <label>
+                    <x-rapidez::label>@lang('Admin password')</x-rapidez::label>
+                    <x-rapidez::input v-model="login.password" name="password" type="password" required/>
+                </label>
+                <label>
+                    <x-rapidez::label>@lang('Customer email')</x-rapidez::label>
+                    <x-rapidez::input v-model="login.customer" name="customer" type="email" required/>
+                </label>
                 <x-rapidez::button type="submit" class="mt-5">
                     @lang('Login as customer')
                 </x-rapidez::button>
