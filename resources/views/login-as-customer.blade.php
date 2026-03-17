6405bb41dca0cc6b11d8c70ac113de16df59d338
Vincent Boon <vincent@justbetter.nl>
2026-03-17 13:23:09 +0100

    Tailwind 4 update (#21)

--- resources/views/login-as-customer.blade.php
+++ resources/views/login-as-customer.blade.php
@@ -9,7 +9,7 @@
         <h1 class="font-bold text-4xl mb-5">@lang('Login as customer')</h1>
         @if($token ?? '') 
             <login-as-customer-by-token token="{{ $token }}" v-slot="{ processing }">
-                <div v-cloak v-if="processing" class="bg-black bg-opacity-50 z-50 fixed inset-0 flex justify-items-center items-center">
+                <div v-cloak v-if="processing" class="bg-black/50 z-50 fixed inset-0 flex justify-items-center items-center">
                     <div class="flex-row justify-items-center items-center mx-auto">
                         <x-rapidez-loading class="size-10 text-gray-200 animate-spin fill-primary" />
                         <span class="text-white">@lang('Logging in...')</span>
@@ -19,7 +19,7 @@
         @endif
 
         <login-as-customer v-slot="login" v-cloak>
-            <form class="p-8 border rounded w-[400px]" v-on:submit.prevent="login.login">
+            <form class="p-8 border rounded-sm w-[400px]" v-on:submit.prevent="login.login">
                 <label>
                     <x-rapidez::label>@lang('Admin username')</x-rapidez::label>
                     <x-rapidez::input v-model="login.username" name="username" required/>
